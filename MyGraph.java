9d54491f723df6ebe46917ce7f65d7c02d3d8589
Lgonzalez36 <luis.gonzalez232@icloud.com>
2020-04-09 13:34:40 -0500

    short, helpful message

---
+++ MyGraph.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class MyGraph implements Graph210 {
+
+    @Override
+    public int numVerts() {
+        return 0;
+    }
+
+    @Override
+    public int numEdges() {
+        return 0;
+    }
+
+    @Override
+    public Edge210 addEdge(int v1, int v2, double weight) {
+        return null;
+    }
+
+    @Override
+    public List<Edge210> edges(int v) {
+        return null;
+    }
+
+    @Override
+    public List<Edge210> allEdges() {
+        return null;
+    }
+
+}
